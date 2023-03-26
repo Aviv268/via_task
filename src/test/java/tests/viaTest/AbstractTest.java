974867ee4d5fc4533c2d9f643d0cb83fe786fc33
AvivAlmoznino <aviv.almoznino@plainid.com>
2023-03-26 13:42:03 +0300

    updating the AbstractTest

--- src/test/java/tests/viaTest/AbstractTest.java
+++ src/test/java/tests/viaTest/AbstractTest.java
@@ -1,8 +1,37 @@
 package tests.viaTest;
 
+import com.microsoft.playwright.*;
+import org.testng.annotations.AfterClass;
+import org.testng.annotations.BeforeClass;
+import pageObjects.ComplicatedPage;
+import pageObjects.HeaderMenuPage;
 import webDriver.ActionBot;
 
 public class AbstractTest {
 
+    private Playwright playwright;
+    private Browser browser;
+    private BrowserContext context;
+    private Page page;
+    private ComplicatedPage complicatedPage;
+    private HeaderMenuPage headerMenuPage;
+
     protected ActionBot bot;
+    private static final String PAGE_URL = "https://ultimateqa.com/complicated-page";
+
+    @BeforeClass
+    public void setUp() {
+        playwright = Playwright.create();
+        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setSlowMo(400).setHeadless(false));
+        context = browser.newContext();
+        page = context.newPage();
+        bot = new ActionBot(page); // Initialize the ActionBot instance
+        complicatedPage = new ComplicatedPage(bot);
+        page.navigate(PAGE_URL);
+    }
+
+    @AfterClass
+    public void tearDown() {
+        playwright.close();
+    }
 }
--- src/test/java/tests/viaTest/ComplicatedPageTest.java
+++ src/test/java/tests/viaTest/ComplicatedPageTest.java
@@ -1,41 +1,19 @@
 package tests.viaTest;
 
-import com.microsoft.playwright.*;
 import org.testng.Assert;
-import org.testng.annotations.AfterClass;
-import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
 import pageObjects.ComplicatedPage;
 import pageObjects.ContactUsPage;
 import pageObjects.HeaderMenuPage;
-import webDriver.ActionBot;
 
 public class ComplicatedPageTest extends AbstractTest {
 
-    private Playwright playwright;
-    private Browser browser;
-    private BrowserContext context;
-    private Page page;
-    private ComplicatedPage complicatedPage;
-    private HeaderMenuPage headerMenuPage;
-
-    private static final String PAGE_URL = "https://ultimateqa.com/complicated-page";
     private static final int EXPECTED_BUTTON_COUNT = 12;
-    private static final String EXPECTED_CONTACTUS_HEADER = "Contact Us";
-
-    @BeforeClass
-    public void setUp() {
-        playwright = Playwright.create();
-        browser = playwright.chromium().launch(new BrowserType.LaunchOptions().setHeadless(false));
-        context = browser.newContext();
-        page = context.newPage();
-        bot = new ActionBot(page); // Initialize the ActionBot instance
-        complicatedPage = new ComplicatedPage(bot);
-        page.navigate(PAGE_URL);
-    }
+    private static final String EXPECTED_CONTACTUS_HEADER = "Contact Us1";
 
     @Test(priority = 1)
     public void testButtonCount() {
+        ComplicatedPage complicatedPage = new ComplicatedPage(bot);
         complicatedPage.waitForPageLoad();
         int actualButtonCount = complicatedPage.getButtonCount();
         Assert.assertEquals(actualButtonCount, EXPECTED_BUTTON_COUNT, "Button count doesn't match expected count");
@@ -43,6 +21,7 @@ public class ComplicatedPageTest extends AbstractTest {
 
     @Test(priority = 2)
     public void testFacebookButtonsHref() {
+        ComplicatedPage complicatedPage = new ComplicatedPage(bot);
         complicatedPage.waitForPageLoad();
         boolean isFacebookHrefEqual = complicatedPage.isFacebookHrefEqual();
         Assert.assertTrue(isFacebookHrefEqual, "Facebook href doesn't match expected href");
@@ -50,6 +29,7 @@ public class ComplicatedPageTest extends AbstractTest {
 
     @Test(priority = 3)
     public void testContactFormSubmission() {
+        ComplicatedPage complicatedPage = new ComplicatedPage(bot);
         complicatedPage.waitForPageLoad();
         complicatedPage.fillContactForm("Test User", "test@example.com", "Test message");
         complicatedPage.submitContactForm();
@@ -63,11 +43,6 @@ public class ComplicatedPageTest extends AbstractTest {
         ContactUsPage contactUsPage = headerPage.clickOnContactUsBtn();
         String contactUsHeader = contactUsPage.getHeaderTextContext();
         Assert.assertEquals(contactUsHeader, EXPECTED_CONTACTUS_HEADER);
-    }
-
-    @AfterClass
-    public void tearDown() {
-        playwright.close();
     }
 
 }
